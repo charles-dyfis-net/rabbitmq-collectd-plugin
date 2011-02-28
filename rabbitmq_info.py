0cd7a68d72388325486e502ab3700321a209cd06
Charles Duffy <charles@tippr.com>
2011-02-27 20:23:34 -0600

    iterate over vhosts; emit statistics per-queue

--- rabbitmq_info.py
+++ rabbitmq_info.py
@@ -8,6 +8,7 @@
 
 
 import collectd
+import collections
 import subprocess
 import re
 
@@ -25,65 +26,68 @@ VERBOSE_LOGGING = False
 
 # Obtain the interesting statistical info
 def get_stats():
-    stats = {}
-    stats['ctl_messages'] = 0
-    stats['ctl_memory'] = 0
-    stats['ctl_consumers'] = 0
-    stats['pmap_mapped'] = 0
-    stats['pmap_used'] = 0
-    stats['pmap_shared'] = 0
+    stats = collections.defaultdict(int)
 
-    # call rabbitmqctl
     try:
-        p = subprocess.Popen([RABBITMQCTL_BIN, 'list_queues', 'messages', 'memory', 'consumers'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        p = subprocess.Popen([RABBITMQCTL_BIN, 'list_vhosts', '-q'], shell=False, stdout=subprocess.PIPE)
     except:
         logger('err', 'Failed to run %s' % RABBITMQCTL_BIN)
         return None
 
-    for line in p.stdout.readlines():
-        if re.match('\d', line):
-            ctl_stats = line.split()
-            stats['ctl_messages'] += int(ctl_stats[0])
-            stats['ctl_memory'] += int(ctl_stats[1])
-            stats['ctl_consumers'] += int(ctl_stats[2])
+    vhosts = p.communicate()[0].split('\n')
 
-    if not stats['ctl_memory'] > 0:
-        logger('warn', '%s reports 0 memory usage. This is probably incorrect.' % RABBITMQCTL_BIN)
+    for vhost in vhosts:
+        # call rabbitmqctl
+        try:
+            p = subprocess.Popen([RABBITMQCTL_BIN, 'list_queues', '-q', '-p', vhost, 'name', 'messages', 'memory', 'consumers'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        except:
+            logger('err', 'Failed to run %s' % RABBITMQCTL_BIN)
+            return None
 
-    # get the pid of rabbitmq (beam.smp)
-    try:
-        p = subprocess.Popen([PIDOF_BIN, 'beam.smp'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    except:
-        logger('err', 'Failed to run %s' % PIDOF_BIN)
-        return None
 
-    line = p.stdout.read().strip()
-    if not re.search('\D', line):
-        pid = line
-    else:
-        logger('err', '%s returned something strange.' % PIDOF_BIN)
-        return None
 
-    # use pmap to get proper memory stats
-    try:
-        p = subprocess.Popen([PMAP_BIN, '-d', pid], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    except:
-        logger('err', 'Failed to run %s' % PMAP_BIN)
-        return None
+        for line in p.stdout.readlines():
+            name, messages, memory, consumers = line.split()
+            stats['%s.%s.ctl_messages' % (vhost, name)] = messages
+            stats['%s.%s.ctl_memory' % (vhost, name)] = memory
+            stats['%s.%s.ctl_consumers' % (vhost, name)] = consumers
+            if not memory > 0:
+                logger('warn', '%s reports 0 memory usage for %s.%s. This is probably incorrect.' % (RABBITMQCTL_BIN, vhost, name))
 
-    line = p.stdout.readlines()[-1].strip()
-    if re.match('mapped', line):
-        m = re.match(r"\D+(\d+)\D+(\d+)\D+(\d+)", line)
-        stats['pmap_mapped'] = int(m.group(1))
-        stats['pmap_used'] = int(m.group(2))
-        stats['pmap_shared'] = int(m.group(3))
-    else:
-        logger('warn', '%s returned something strange.' % PMAP_BIN)
-        return None
-        
-    # Verbose output
-    logger('verb', '[rmqctl] Messages: %i, Memory: %i, Consumers: %i' % (stats['ctl_messages'], stats['ctl_memory'], stats['ctl_consumers']))
-    logger('verb', '[pmap] Mapped: %i, Used: %i, Shared: %i' % (stats['pmap_mapped'], stats['pmap_used'], stats['pmap_shared']))
+        # get the pid of rabbitmq (beam.smp)
+        try:
+            p = subprocess.Popen([PIDOF_BIN, 'beam.smp'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        except:
+            logger('err', 'Failed to run %s' % PIDOF_BIN)
+            return None
+
+        line = p.stdout.read().strip()
+        if not re.search('\D', line):
+            pid = line
+        else:
+            logger('err', '%s returned something strange.' % PIDOF_BIN)
+            return None
+
+        # use pmap to get proper memory stats
+        try:
+            p = subprocess.Popen([PMAP_BIN, '-d', pid], shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        except:
+            logger('err', 'Failed to run %s' % PMAP_BIN)
+            return None
+
+        line = p.stdout.readlines()[-1].strip()
+        if re.match('mapped', line):
+            m = re.match(r"\D+(\d+)\D+(\d+)\D+(\d+)", line)
+            stats['pmap_mapped'] = int(m.group(1))
+            stats['pmap_used'] = int(m.group(2))
+            stats['pmap_shared'] = int(m.group(3))
+        else:
+            logger('warn', '%s returned something strange.' % PMAP_BIN)
+            return None
+            
+        # Verbose output
+        logger('verb', '[rmqctl] Messages: %i, Memory: %i, Consumers: %i' % (stats['ctl_messages'], stats['ctl_memory'], stats['ctl_consumers']))
+        logger('verb', '[pmap] Mapped: %i, Used: %i, Shared: %i' % (stats['pmap_mapped'], stats['pmap_used'], stats['pmap_shared']))
 
     return stats
 
